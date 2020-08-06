64cb4d8567f0b64db3d76f334d49bdd73804b58f
Muhammad Areeb Kazmi <54742430+MuhammadAreebKazmi@users.noreply.github.com>
2020-08-06 16:01:51 +0500

    VB code for 10 employees

---
+++ Records for 10 men.vb
@@ -0,0 +1,31 @@
+﻿Module Module1
+    Structure EMPLOYEE
+        Dim HoursWorked As Integer
+        Dim EmpID As String
+        Dim PerHourRate As Integer
+        Dim AmountToBePaid As Integer
+    End Structure
+
+    Sub Main()
+        Dim Employee(10) As EMPLOYEE
+        Dim index As Integer = 0
+
+        For index = 1 To 10
+            Console.Write("Enter hours worked:")
+            Employee(index).HoursWorked = Console.ReadLine
+
+            Console.Write("Enter EmplyerID")
+            Employee(index).EmpID = Console.ReadLine
+
+            Console.Write("Enter Perhour rate")
+            Employee(index).PerHourRate = Console.ReadLine
+
+            Employee(index).AmountToBePaid = Employee(index).HoursWorked * Employee(index).PerHourRate
+
+
+            Console.WriteLine(Employee(index).EmpID & " " & Employee(index).HoursWorked & " " & Employee(index).PerHourRate & " " & Employee(index).AmountToBePaid)
+        Next
+        Console.ReadKey()
+    End Sub
+
+End Module
